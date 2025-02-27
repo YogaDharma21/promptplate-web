0d40dfb4953ed972f5eede5be169ed1388f26730
YogaDharma21 <yogacode2006@gmail.com>
2025-02-27 19:15:17 +0800

    test: update after submodule

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
         <div className="flex min-h-screen flex-col items-center justify-center p-4 text-white">
             <main className="text-center flex flex-col gap-5">
                 <h1 className="bg-gradient-to-r from-neutral-200 to-neutral-400 bg-clip-text text-5xl font-bold text-transparent md:text-6xl">
-                    Prompt Plate
+                    Prompt Plate.
                 </h1>
 
                 <p className="mx-auto max-w-md text-xl text-neutral-400 md:text-2xl">
